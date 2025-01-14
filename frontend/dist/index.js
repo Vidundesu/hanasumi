2ca47776b858bd5158f3fe4989c6bcb7b4274988
Vidun Nethdina <vinethdina77@gmail.com>
2025-01-14 23:19:33 +0530

    frontend

--- frontend/dist/index.js
+++ frontend/dist/index.js
@@ -3,3 +3,8 @@ $(document).ready(function () {
         console.log("Navbar loaded successfully.");
     });
 });
+$(document).ready(function () {
+    $("#footer-placeholder").load("footer.html", function () {
+        console.log("Footer loaded successfully.");
+    });
+});
